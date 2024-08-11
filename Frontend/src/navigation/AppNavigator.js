11f508ca83c5b92de669efce53679af835bb59e6
Ikshitha J <ikshi.thaj0477@gmail.com>
2024-08-11 19:18:00 +0530

    added community launch page

--- Frontend/src/navigation/AppNavigator.js
+++ Frontend/src/navigation/AppNavigator.js
@@ -4,16 +4,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
 import GratitudeJarScreen from '../screens/GratitudeJarScreens/GratitudeJarScreen';
+import CommunityScreen from '../screens/Community/index';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-        <Stack.Screen name="GratitudeJar" component={GratitudeJarScreen} options={{ title: 'Gratitude Jar' }} />
-      </Stack.Navigator>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+          <Stack.Screen name="GratitudeJar" component={CommunityScreen} options={{ title: 'Comunity' }} />
+        </Stack.Navigator>
     </NavigationContainer>
   );
 };
