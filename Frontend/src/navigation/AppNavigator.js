81385e9a51c4dd3b4bfe55e2ac7c5212162f845f
Sahiti Vempalli <vsahiti92005@gmail.com>
2024-08-15 22:12:30 +0530

    Enhanced gratitude jar

--- Frontend/src/navigation/AppNavigator.js
+++ Frontend/src/navigation/AppNavigator.js
@@ -1,9 +1,10 @@
-
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
 import GratitudeJarScreen from '../screens/GratitudeJarScreens/GratitudeJarScreen';
+import GratitudeListScreen from '../screens/GratitudeJarScreens/GratitudeList';
+import AddGratitudeScreen from '../screens/GratitudeJarScreens/AddGratitude';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,8 @@ const AppNavigator = () => {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
         <Stack.Screen name="GratitudeJar" component={GratitudeJarScreen} options={{ title: 'Gratitude Jar' }} />
+        <Stack.Screen name="GratitudeList" component={GratitudeListScreen} options={{ title: 'Your Gratitude' }} />
+        <Stack.Screen name="AddGratitude" component={AddGratitudeScreen} options={{ title: 'Add Gratitude' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
