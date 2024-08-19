74b75f5298ec956959cc6b6ff09acbeab54e0f7c
Ikshitha J <ikshi.thaj0477@gmail.com>
2024-08-19 14:30:59 +0530

    updated homescreen

--- Frontend/src/navigation/AppNavigator.js
+++ Frontend/src/navigation/AppNavigator.js
@@ -1,82 +1,26 @@
-// import React from 'react';
-// import { createStackNavigator } from '@react-navigation/stack';
- import HomeScreen from '../screens/HomeScreen';
- import GratitudeJarScreen from '../screens/GratitudeJarScreens/GratitudeJarScreen';
- import NotesScreen from '../screens/GratitudeJarScreens/NotesScreen';
- import AddNoteScreen from '../screens/GratitudeJarScreens/AddNoteScreen';
-
-// const Stack = createStackNavigator();
-
-// const AppNavigator = () => {
-//   return (
-//     <Stack.Navigator initialRouteName="Home">
-//       {/* <Stack.Screen name="Home" component={HomeScreen} />
-//       <Stack.Screen name="GratitudeJarScreen" component={GratitudeJarScreen} />
-//       <Stack.Screen name="NotesScreen" component={NotesScreen} />
-//       <Stack.Screen name="AddNoteScreen" component={AddNoteScreen} /> */}
-//       <Stack.Screen 
-//       name="Home" 
-//       component={HomeScreen} 
-//       options={{ title: 'Home' }} 
-//     />
-//     <Stack.Screen 
-//     name="GratitudeJarScreen" 
-//     component={GratitudeJarScreen} 
-//     options={{ title: 'Gratitude Jar' }} 
-//     />
-//   <Stack.Screen 
-//   name="NotesScreen" 
-//   component={NotesScreen} 
-//   options={{ title: 'Your Memories' }} 
-//   />
-// <Stack.Screen 
-//   name="AddNoteScreen" 
-//   component={AddNoteScreen} 
-//   options={{ title: 'Add New Note' }} 
-// />
-
-//     </Stack.Navigator>
-//   );
-// };
-
-// export default AppNavigator;
-//My working version above
-
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "../screens/LogScreens/LoginScreen";
+import CalendarScreen from "../screens/Trackers/MoodTrackCal";
 const Stack = createStackNavigator();
+
 const AppNavigator = () => {
   return (
-
+    <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Login"
           component={LoginScreen}
           options={{ title: "Login" }}
         />
-      <Stack.Screen 
-      name="Home" 
-      component={HomeScreen} 
-      options={{ title: 'Home' }} 
-    />
-    <Stack.Screen 
-    name="GratitudeJarScreen" 
-    component={GratitudeJarScreen} 
-    options={{ title: 'Gratitude Jar' }} 
-    />
-  <Stack.Screen 
-  name="NotesScreen" 
-  component={NotesScreen} 
-  options={{ title: 'Your Memories' }} 
-  />
-<Stack.Screen 
-  name="AddNoteScreen" 
-  component={AddNoteScreen} 
-  options={{ title: 'Add New Note' }} 
-/>
-
-    </Stack.Navigator>
+        <Stack.Screen
+          name="Calendar"
+          component={CalendarScreen}
+          options={{ title: "Calendar" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
